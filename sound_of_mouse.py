fddfa16728d8211cdafb08248ba9cd75498832a8
Dmitry Lvovich Grachev <grdmitriyl@gmail.com>
2022-11-17 15:53:56 +0300

    bug fix: fix crash of the app 'OSError: [Errno -9993] Illegal combination of I/O devices'

--- sound_of_mouse.py
+++ sound_of_mouse.py
@@ -6,32 +6,35 @@ from time import sleep
 
 def main(mouse_position: tuple):
 
-    p = pyaudio.PyAudio()     #initialize pyaudio
     BITRATE = 5000     #number of frames per second/frameset.
-    FREQUENCY = int((mouse_position[0] * mouse_position[1]) / 103.68)  # Hz
-    FREQUENCY = FREQUENCY if FREQUENCY != 0 else 1
-    LENGTH = .1    #seconds to play sound
+    LENGTH = .15    #seconds to play sound
 
-    if FREQUENCY > BITRATE:
-        BITRATE = FREQUENCY + 100
+    frequency = int((mouse_position[0] * mouse_position[1]) / 103.68)  # Hz
+    frequency = frequency if frequency != 0 else 1
+
+    if frequency > BITRATE:
+        BITRATE = frequency + 100
     NUMBEROFFRAMES = int(BITRATE * LENGTH)
     RESTFRAMES = NUMBEROFFRAMES % BITRATE
     WAVEDATA = ''
 
     #generating waves
     for x in range(1, NUMBEROFFRAMES + 1):
-        WAVEDATA += chr(int(math.sin(x / ( (BITRATE/FREQUENCY) / math.pi )) * 127 + 128))
+        WAVEDATA += chr(int(math.sin(x / ( (BITRATE/frequency) / math.pi )) * 127 + 128))
     for x in range(RESTFRAMES):
         WAVEDATA = WAVEDATA + chr(128)
 
+    p = pyaudio.PyAudio()     #initialize pyaudio
     stream = p.open(format = p.get_format_from_width(1),
-                    channels = 2,
+                    channels = 1,
                     rate = BITRATE,
                     output = True)
     stream.write(WAVEDATA)
-    #stream.stop_stream()
-    #stream.close()
-    #p.terminate()
+
+    stream.start_stream()
+    stream.stop_stream()
+    stream.close()
+    # p.terminate()
 
 
 if __name__ == "__main__":
